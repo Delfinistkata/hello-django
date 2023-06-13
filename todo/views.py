7f273e81b1862d4bc99c1c29b292a4bf6c0ad76b
Delfinistkata <elidimitrovadkit@gmail.com>
2023-06-13 12:05:54 +0000

    Add data to the temlate

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,28 @@
+"""
+Module: views.py
+Description: This module defines the views for the todo application.
+"""
+
 from django.shortcuts import render
+from .models import Item
 
 
 # Create your views here.
 
 def get_todo_list(request):
-    return render(request, 'todo/todo_list.html')
 
+    """
+    View function for rendering the todo list page.
+
+    Parameters:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered HTML response.
+    """
+
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
