e45913dda61e490c898340d48298ff727f6be594
Delfinistkata <elidimitrovadkit@gmail.com>
2023-06-13 11:21:36 +0000

    Add modules to admin panel

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,11 @@
+"""
+Module: admin.py
+Description: This module registers the Item model in the Django admin site.
+"""
+
 from django.contrib import admin
+from .models import Item
+
 
 # Register your models here.
+admin.site.register(Item)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,31 @@
+"""
+Module: models.py
+Description: This module defines the Item model for the application.
+"""
+
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+
+    """
+    Model representing an item.
+
+    This model stores information about an item,
+    including its name and status of completion.
+
+    Attributes:
+        name (CharField): The name of the item.
+        done (BooleanField): The status of completion of the item.
+
+    Methods:
+        __str__(): Returns the string representation of the item.
+    """
+
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False)
+
+    def __str__(self):
+        return str(self.name)
